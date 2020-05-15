8cab6d016d13f7a829aac62a3ddc20fbeda36f33
littlearmour <1375524732@qq.com>
2020-05-15 19:19:31 +0800

    init

---
+++ frank-com/public/frank.js
@@ -0,0 +1,23 @@
+function jsonp(url) {
+  return new Promise((resolve, reject) => {
+    const random = "frankJSONPCallback" + Math.random();
+    console.log(random);
+    window[random] = (data) => {
+      resolve(data);
+    };
+    const script = document.createElement("script");
+    script.src = `${url}?callback=${random}`;
+
+    script.onload = () => {
+      script.remove();
+    };
+    script.onerror = () => {
+      reject();
+    };
+    document.body.appendChild(script);
+  });
+}
+
+jsonp("http://qq.com:8888/friends.js").then((data) => {
+  console.log(data);
+});
